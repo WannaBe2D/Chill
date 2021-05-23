66c82f53440b9c6d76833fb304a8ab87f7c3f1c4
WannaBe2D <nikolayskochko03@gmail.com>
2021-05-23 16:54:23 +0300

    customize volume

--- chill-frontend/index.js
+++ chill-frontend/index.js
@@ -17,16 +17,18 @@ function buildSound(i) {
         for(let n=0;n<list.length;n++){
             if(list[n].id == i){
                 let item = `
-                <figure style="padding-bottom: 30px; border-bottom: 1px solid black;width:280px;font-size: 14px;">
+                <figure style="padding-bottom: 30px;width:280px;font-size: 14px;">
                     <figcaption>${list[n].name}  <button class="btn-nav" id="pl-${list[n].id}" onclick="controlPlay(${list[n].id})"><img id="im-${list[n].id}" src="free-icon-play-button-153752.png"></button></figcaption>
-                    <audio id="audio-${list[n].id}"
-                       
+                    <audio id="audio-${list[n].id}" loop
+
                         src="${list[n].music}">
                             Your browser does not support the
                             <code>audio</code> element.
                     </audio>
+                    <input type="range" id="volume-${list[n].id}" name="volume" min="0" max="100" value="50" step="1" onmousemove="setVolume(${list[n].id})">
                 </figure>`
-                wrapper.innerHTML += item
+                
+                wrapper.innerHTML += item          
             }
             
         }
@@ -88,6 +90,7 @@ mySpoti.onclick = () => {
     let newValue = spotUrl + val.split('com')[1]
 
     document.getElementById('spoti-play').setAttribute('src', newValue)
+    document.getElementById('form-url').value = ''
 }
 
 
@@ -103,4 +106,15 @@ function controlPlay(a){
         myAudio.pause()
         mybutAudio.setAttribute('src', 'free-icon-play-button-153752.png')
     }    
+}
+
+
+// Volume
+
+function setVolume(a) {
+    let audio = document.getElementById("audio-" + String(a))
+    let volume = document.getElementById("volume-" + String(a))
+
+    audio.volume = volume.value / 100
+
 }
